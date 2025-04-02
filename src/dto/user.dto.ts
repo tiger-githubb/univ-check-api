4ce4ad8c8d37b8035d4a0ede8cf5cb729488b466
alwil17 <willialfred24@gmail.com>
2025-04-02 15:47:57 +0000

    Added services

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,13 +1,42 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { RoleEnum } from "../entity/User.entity";
 
-export class CreateUserDTO {
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   phone: string;
-  role: RoleEnum;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  role: RoleEnum;
 }
+
 export type Payload = {
   id?: string;
   email: string;
 };
+
+
+
+export class LoginDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
