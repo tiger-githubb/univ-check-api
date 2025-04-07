d85391714a079f228abedd4d8eed82d93a107b96
Jean-Gaël <61506484+Alwil17@users.noreply.github.com>
2025-04-07 21:51:22 +0000

    Thinks it's working (#22)
    * Typescript api initialzed
    
    * Update README
    
    * Add license file
    
    * Task in progress
    
    * Project task completed
    
    * Task in progress
    
    * Auth management + basic seed task completed
    
    * Add middleware with roles guard
    
    * Update README file
    
    * Update tsconfig
    
    * Reproduced entities
    
    * improved package.json
    
    * just changed name for more flexibility
    
    * Fixed datasource to run migrations
    
    * Added services
    
    * Added class-validator and Dtos
    
    * Added controller
    
    * Added routes
    
    * Pushed router to index.ts
    
    * Added and tested seeder
    
    * Testing swagger integration
    
    * Merging local devs to master (#4)
    
    * Dev (#2)
    
    * Typescript api initialzed
    
    * Update README
    
    * Add license file
    
    * Task in progress
    
    * Project task completed
    
    * Task in progress
    
    * Auth management + basic seed task completed
    
    * Add middleware with roles guard
    
    * Update README file
    
    * Update tsconfig
    
    ---------
    
    Co-authored-by: mbradiouf14 <mbradiouf14@gmail.com>
    Co-authored-by: bradlab <37481556+bradlab@users.noreply.github.com>
    
    * Reproduced entities
    
    * improved package.json
    
    * just changed name for more flexibility
    
    * Fixed datasource to run migrations
    
    * Added services
    
    * Added class-validator and Dtos
    
    * Added controller
    
    * Added routes
    
    * Pushed router to index.ts
    
    * Added and tested seeder
    
    * Testing swagger integration
    
    ---------
    
    Co-authored-by: KARBOU Aristide <aristidetiger12@gmail.com>
    Co-authored-by: mbradiouf14 <mbradiouf14@gmail.com>
    Co-authored-by: bradlab <37481556+bradlab@users.noreply.github.com>
    
    * Reproduced entities
    
    * improved package.json
    
    * just changed name for more flexibility
    
    # Conflicts:
    #	src/config/data-source.ts
    #	src/controllers/auth.controller.ts
    #	src/controllers/user.controllers.ts
    #	src/dto/user.dto.ts
    #	src/index.ts
    #	src/middleware/authorization.ts
    #	src/routes/user.routes.ts
    dd
    
    * Added services
    
    * Added class-validator and Dtos
    
    * Added controller
    
    * Added routes
    
    * Pushed router to index.ts
    
    * Added and tested seeder
    
    * Init
    
    * Merged completely with devmain
    
    * User entity rename file
    
    * User entity removed
    
    * Rename Subject to Course + Add auth and users swagger doc
    
    * Add swagger for all models and routes
    
    * Add DTO validator on Auth and User routes
    
    * Fixed env variables
    
    * Added vercel support
    
    * Mind delete yarn
    
    * Delete yarn.lock
    
    * Adapting ts
    
    * Testing in local
    
    * Fixed vercel.json
    
    * servless ready
    
    * Testing gpt fix
    
    * Desparate fix
    
    * Desparate fix
    
    * servless ready
    
    * servless ready
    
    * Fix all GPT mistakes
    
    * Update index.ts
    
    * Added servless
    
    * Forced npm on vercel
    
    * Tested vercel dev and it's working
    
    * Tested vercel node
    
    * Tested vercel node
    
    * Tsting swaggr build
    
    * Tested vercel node
    
    * Tested vercel node
    
    * Tested vercel node
    
    * Trying to see doc
    
    * Try to bypass neon
    
    * another test
    
    ---------
    
    Co-authored-by: mbradiouf14 <mbradiouf14@gmail.com>
    Co-authored-by: bradlab <37481556+bradlab@users.noreply.github.com>
    Co-authored-by: KARBOU Aristide <aristidetiger12@gmail.com>

--- api/index.ts
+++ api/index.ts
@@ -1,31 +1,33 @@
-import {connectDB} from "./config/data-source";
+import {connectDB} from "../src/config/data-source";
 const express = require("express");
 import { Request, Response } from "express";
 import * as dotenv from "dotenv";
 import "reflect-metadata";
-import { errorHandler } from "./middleware/errorHandler";
-import { authRouter } from "./routes/auth.routes";
+import { errorHandler } from "../src/middleware/errorHandler";
+import { authRouter } from "../src/routes/auth.routes";
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 import * as fs from "fs";
+import path from "path";
 
 // Importer les routes
-import organisationRouter from './routes/organisation.routes';
-import universiteRouter from './routes/universite.routes';
-import departementRouter from './routes/departement.routes';
-import programmeRouter from './routes/programme.routes';
-import academicYearRouter from './routes/academicYear.routes';
-import courseRouter from './routes/course.routes';
-import classSessionRouter from './routes/classSession.routes';
-import userRouter from './routes/user.routes';
-import emargementRouter from './routes/emargement.routes';
-import notificationRouter from './routes/notification.routes';
+import organisationRouter from '../src/routes/organisation.routes';
+import universiteRouter from '../src/routes/universite.routes';
+import departementRouter from '../src/routes/departement.routes';
+import programmeRouter from '../src/routes/programme.routes';
+import academicYearRouter from '../src/routes/academicYear.routes';
+import courseRouter from '../src/routes/course.routes';
+import classSessionRouter from '../src/routes/classSession.routes';
+import userRouter from '../src/routes/user.routes';
+import emargementRouter from '../src/routes/emargement.routes';
+import notificationRouter from '../src/routes/notification.routes';
 
 dotenv.config();
 const app = express();
 const globalPath = '/api/v1';
 
-const swaggerDocument = JSON.parse(fs.readFileSync("./swagger.json", "utf8"));
+const swaggrFilePath = path.join(__dirname, "swagger.json");
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggrFilePath, "utf8"));
 const swaggerOptions = {
     definition: swaggerDocument,
     apis: ["./src/routes/*.ts"], // ajouter des routes documentées ici
@@ -63,3 +65,6 @@ app.use(errorHandler);
 app.get("*", (req: Request, res: Response) => {
     res.status(505).json({ message: "Bad Request" });
 });
+
+
+module.exports = app;
--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,49 +1,58 @@
 import "reflect-metadata"
-import { DataSource } from "typeorm";
+import {DataSource} from "typeorm";
 import * as dotenv from "dotenv";
-import { AcademicYear } from "../entity/AcademicYear.entity";
-import { Organisation } from "../entity/Organisation.entity";
-import { Universite } from "../entity/Universite.entity";
-import { Departement } from "../entity/Departement.entity";
-import { Programme } from "../entity/Programme.entity";
-import { ClassSession } from "../entity/ClassSession.entity";
-import { Course } from "../entity/Course.entity";
-import { Emargement } from "../entity/Emargement.entity";
-import { Notification } from "../entity/Notification.entity";
-import { User } from "../entity/User.entity";
+import {AcademicYear} from "../entity/AcademicYear.entity";
+import {Organisation} from "../entity/Organisation.entity";
+import {Universite} from "../entity/Universite.entity";
+import {Departement} from "../entity/Departement.entity";
+import {Programme} from "../entity/Programme.entity";
+import {ClassSession} from "../entity/ClassSession.entity";
+import {Course} from "../entity/Course.entity";
+import {Emargement} from "../entity/Emargement.entity";
+import {Notification} from "../entity/Notification.entity";
+import {User} from "../entity/User.entity";
+import {parse} from "pg-connection-string";
 
 dotenv.config()
+
+const databaseURL = process.env.DATABASE_URL || process.env.POSTGRES_URL;
+const config = parse(databaseURL);
 
 const isDev = process.env.NODE_ENV === "dev"
 
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL || process.env.POSTGRES_URL,
-  synchronize: isDev,
-  logging: isDev,
-  ssl: !isDev ? { rejectUnauthorized: false } : false, // important pour Neon
-  entities: [
-    Organisation,
-    Universite,
-    Departement,
-    Programme,
-    AcademicYear,
-    ClassSession,
-    Course,
-    Emargement,
-    Notification,
-    User,
-  ],
-  migrations: [__dirname + "/migration/*.ts"],
-  subscribers: [],
+    type: "postgres",
+    host: config.host,
+    username: config.user,
+    password: config.password,
+    database: config.database,
+    synchronize: isDev,
+    logging: isDev,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    entities: [
+        Organisation,
+        Universite,
+        Departement,
+        Programme,
+        AcademicYear,
+        ClassSession,
+        Course,
+        Emargement,
+        Notification,
+        User,
+    ],
+    migrations: [__dirname + "/migration/*.ts"],
+    subscribers: [],
 });
 
 export const connectDB = async () => {
-  try {
-    await AppDataSource.initialize();
-    console.log("Database connected successfully!");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-  }
+    try {
+        await AppDataSource.initialize();
+        console.log("Database connected successfully!");
+    } catch (error) {
+        console.error("Database connection failed:", error);
+    }
 };
