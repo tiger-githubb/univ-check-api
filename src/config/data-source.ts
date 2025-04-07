f3dde2f434289067c04911721cbcca7af6cc337d
alwil17 <willialfred24@gmail.com>
2025-04-07 21:25:50 +0000

    Try to bypass neon

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,16 +1,16 @@
 import "reflect-metadata"
-import { DataSource } from "typeorm";
+import {DataSource} from "typeorm";
 import * as dotenv from "dotenv";
-import { AcademicYear } from "../entity/AcademicYear.entity";
-import { Organisation } from "../entity/Organisation.entity";
-import { Universite } from "../entity/Universite.entity";
-import { Departement } from "../entity/Departement.entity";
-import { Programme } from "../entity/Programme.entity";
-import { ClassSession } from "../entity/ClassSession.entity";
-import { Course } from "../entity/Course.entity";
-import { Emargement } from "../entity/Emargement.entity";
-import { Notification } from "../entity/Notification.entity";
-import { User } from "../entity/User.entity";
+import {AcademicYear} from "../entity/AcademicYear.entity";
+import {Organisation} from "../entity/Organisation.entity";
+import {Universite} from "../entity/Universite.entity";
+import {Departement} from "../entity/Departement.entity";
+import {Programme} from "../entity/Programme.entity";
+import {ClassSession} from "../entity/ClassSession.entity";
+import {Course} from "../entity/Course.entity";
+import {Emargement} from "../entity/Emargement.entity";
+import {Notification} from "../entity/Notification.entity";
+import {User} from "../entity/User.entity";
 
 dotenv.config()
 
@@ -18,32 +18,34 @@ const isDev = process.env.NODE_ENV === "dev"
 
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL || process.env.POSTGRES_URL,
-  synchronize: isDev,
-  logging: isDev,
-  ssl: !isDev ? { rejectUnauthorized: false } : false, // important pour Neon
-  entities: [
-    Organisation,
-    Universite,
-    Departement,
-    Programme,
-    AcademicYear,
-    ClassSession,
-    Course,
-    Emargement,
-    Notification,
-    User,
-  ],
-  migrations: [__dirname + "/migration/*.ts"],
-  subscribers: [],
+    type: "postgres",
+    url: process.env.DATABASE_URL || process.env.POSTGRES_URL,
+    synchronize: isDev,
+    logging: isDev,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    entities: [
+        Organisation,
+        Universite,
+        Departement,
+        Programme,
+        AcademicYear,
+        ClassSession,
+        Course,
+        Emargement,
+        Notification,
+        User,
+    ],
+    migrations: [__dirname + "/migration/*.ts"],
+    subscribers: [],
 });
 
 export const connectDB = async () => {
-  try {
-    await AppDataSource.initialize();
-    console.log("Database connected successfully!");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-  }
+    try {
+        await AppDataSource.initialize();
+        console.log("Database connected successfully!");
+    } catch (error) {
+        console.error("Database connection failed:", error);
+    }
 };
